1a02d4aeabbae2418ed895618004f90ee5cb5a41
Lim Swee Tat <st_lim@stlim.net>
2016-04-24 14:35:03 +0800

    Defined tables for books, authors and genres Defined tables author_book and genre_book for many-to-many relationships.

--- database/migrations/2016_04_24_052204_create_books_table.php
+++ database/migrations/2016_04_24_052204_create_books_table.php
@@ -12,10 +12,34 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
+        if (Schema::hasTable('users') &&
+            Schema::hasTable('authors') &&
+            Schema::hasTable('genres')
+        ) {
+            Schema::create('books', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('title');
+                $table->longText('description');
+                $table->date('release_date');
+                $table->foreign('created_by')->references('id')->on('users');
+                $table->foreign('updated_by')->references('id')->on('users');
+                $table->timestamps();
+            });
+            // Define the many to many relationship with authors
+            Schema::create('author_book', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+                $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+                $table->timestamps();
+            });
+            Schema::create('genre_book', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade');
+                $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+                $table->timestamps();
+            });
+
+        }
     }
 
     /**
@@ -25,6 +49,10 @@ class CreateBooksTable extends Migration
      */
     public function down()
     {
-        Schema::drop('books');
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropForeign(['created_by', 'updated_by']);
+        });
+
+        Schema::dropIfExists('books');
     }
 }
--- database/migrations/2016_04_24_052213_create_genres_table.php
+++ database/migrations/2016_04_24_052213_create_genres_table.php
@@ -14,6 +14,8 @@ class CreateGenresTable extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
