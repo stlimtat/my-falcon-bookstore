a1b9e12858d29edc432f6c6915810ece72944418
Lim Swee Tat <st_lim@stlim.net>
2016-04-25 22:43:42 +0800

    Removing search on the sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,20 +111,15 @@
                     </div>
                 @endif
             </div>
-            <!-- search form -->
-            <form action="#" method="get" class="sidebar-form">
-                <div class="input-group">
-                    <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-              </span>
-                </div>
-            </form>
-            <!-- /.search form -->
             <!-- sidebar menu: : style can be found in sidebar.less -->
             <ul class="sidebar-menu">
                 <li class="header">MAIN NAVIGATION</li>
                 <li class="active treeview">
+                    <a href="#">
+                        <i class="fa fa-edit"></i> <span>New Book</span>
+                    </a>
+                </li>
+                <li class="treeview">
                     <a href="#">
                         <i class="fa fa-dashboard"></i> <span>Books</span> <i class="fa fa-angle-left pull-right"></i>
                     </a>
